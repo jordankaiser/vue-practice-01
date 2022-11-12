e6ab5be493495f8e899af50b83fb2d3155968720
codejk <jordan.ross.kaiser@gmail.com>
2022-11-12 19:09:09 +0000

    Update CompositionApiStore.vue

--- src/views/CompositionApiStore.vue
+++ src/views/CompositionApiStore.vue
@@ -2,11 +2,19 @@
   <div>
     <component-a />
     <component-b />
+    <p :class="changeTextColor">Text color change if count > 1</p>
   </div>
 </template>
 
 <script setup>
-import { storeCount } from '../stores/compApiStore'
-import ComponentA from '../components/composition-api-store/componentA.vue';
-import ComponentB from '../components/composition-api-store/componentB.vue';
+import { storeCount } from "../stores/compApiStore";
+import ComponentA from "../components/composition-api-store/componentA.vue";
+import ComponentB from "../components/composition-api-store/componentB.vue";
+import { computed } from "vue";
+
+const changeTextColor = computed(() => {
+  return {
+    orange: storeCount.count > 1,
+  };
+});
 </script>
